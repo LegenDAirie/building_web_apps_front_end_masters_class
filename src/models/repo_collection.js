46ee88d151a099685f5edcb64f69cbd9c8805600
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2016-07-29 16:24:47 -0700

    pulled out the ajaxConfig method from user and turned it into a helper mixin

--- src/models/repo_collection.js
+++ src/models/repo_collection.js
@@ -1,7 +1,8 @@
 import Collection from 'ampersand-rest-collection'
 import Repo from './repo'
+import githubAuthMixin from '../helpers/github_auth_mixin'
 
-export default Collection.extend({
+export default Collection.extend(githubAuthMixin, {
   url: 'https://api.github.com/user/repos',
 
   models: Repo
--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 import Model from 'ampersand-model'
+import githubAuthMixin from 'github_auth_mixin'
 
-export default Model.extend({
+export default Model.extend(githubAuthMixin, {
   url:'https://api.github.com/user',
 
   initialize() {
@@ -22,14 +23,6 @@ export default Model.extend({
   onTokenChange() {
     window.localStorage.token = this.token
     this.fetchInitialData()
-  },
-
-  ajaxConfig () {
-    return {
-      headers: {
-        Authorization: 'token ' + this.token
-      }
-    }
   },
 
   fetchInitialData () {
